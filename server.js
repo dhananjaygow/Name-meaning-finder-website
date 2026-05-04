d460b1a1ba5c39c4d7b92914a7854ca5f070791a
Dhananjay Vikas Patil <dhananjaypatilm51@gmail.com>
2026-05-04 17:23:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const fs = require("fs");
+const csv = require("csv-parser");
+
+const app = express();
+const PORT = 3000;
+
+let namesData = [];
+
+// Load CSV
+fs.createReadStream("names.csv")
+  .pipe(csv())
+  .on("data", (row) => {
+  // skip empty rows
+  if (!row.Name || row.Name.trim() === "") return;
+
+  namesData.push(row);
+})
+  .on("end", () => {
+    console.log("CSV Loaded");
+    console.log(namesData); // DEBUG (you can remove later)
+  });
+
+// API
+app.use(express.static(__dirname));
+app.get("/search", (req, res) => {
+  const input = req.query.name?.toLowerCase();
+
+  if (!input) {
+    return res.json({ message: "No name provided" });
+  }
+
+  const result = namesData.find(
+    (n) => n.Name.toLowerCase() === input
+  );
+
+  if (result) {
+    res.json(result);
+  } else {
+    res.json({ message: "Not found" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
